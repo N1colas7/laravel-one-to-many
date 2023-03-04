7f9fde02a5c1ecd78fbd03ba1cf95babe9fcb560
N1colas7 <nicolasnd9897@gmail.com>
2023-03-04 17:30:31 +0100

    launch seeder Type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +17,16 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        $types = ('HMTL', 'CSS', 'Javascripts','PHP', 'VueJs','SQL', 'Laravel');
+        $types = ['HMTL', 'CSS', 'Javascripts','PHP', 'VueJs','SQL', 'Laravel'];
+
+        foreach($types as $type){
+
+            $newType = new Type();
+
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+
+            $newType->save();
+        }
     }
 }
